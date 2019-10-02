e9e378de0ceebee40e53e935c62aa9e450fe0552
Andries Ngeke <28265548@student.ac.za>
2019-10-02 19:14:12 +0200

    Updated register page with button fuctionality

--- LIGHTSON2/app/src/main/java/com/itrw324/lights_on/RegisterActivity.java
+++ LIGHTSON2/app/src/main/java/com/itrw324/lights_on/RegisterActivity.java
@@ -2,13 +2,43 @@ package com.itrw324.lights_on;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private Button btnReg;
+    private EditText passw,confpassw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnReg = findViewById(R.id.btnRegister);
+        passw  = findViewById(R.id.txtRegPassword);
+        confpassw = findViewById(R.id.txtConfirmPassword);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(passw.getText().toString().equals(confpassw.getText().toString())){
+                    Toast.makeText(getBaseContext(),"Registration success",Toast.LENGTH_SHORT).show();
+                    openHomeActivity();
+                }
+                else {
+                    Toast.makeText(getBaseContext(),"Incorrect confirm password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void openHomeActivity(){
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
     }
 }
