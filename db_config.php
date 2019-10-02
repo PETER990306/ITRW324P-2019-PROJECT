e83488b515c95936e532a978d9072199284a2f6d
LP khambulo <28955765@student.g.nwu.ac.za>
2019-10-02 14:59:14 +0200

    db_config file is used for storing the username, password and database name

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php   // db_config.php
+        define('DB_USER', "user"); // db user
+        define('DB_PASSWORD', "password"); // db password (mention your db password here)
+        define('DB_DATABASE', "db_name"); // database name
+        define('DB_SERVER', "localhost"); // db server
+?>
