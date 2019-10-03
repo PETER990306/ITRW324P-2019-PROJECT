ab7fafd044188e037cbe3d28b03592dba2090067
Teboho-hash <54534135+Teboho-hash@users.noreply.github.com>
2019-10-03 11:43:04 +0200

    Add files via upload

---
+++ main_connect.java
@@ -0,0 +1,17 @@
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LogIn_Activity extends Activity{
+  private Button btnEnter;
+  private TextView lblRegt;
+
+}
