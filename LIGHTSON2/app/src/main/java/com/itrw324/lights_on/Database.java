3c19f9d3e5200c85486034b36f64d1d7a1f922a2
Andries Ngeke <28265548@student.g.nwu.ac.za>
2019-10-03 11:36:30 +0200

    Inserted code for registering users

--- LIGHTSON2/app/src/main/java/com/itrw324/lights_on/Database.java
+++ LIGHTSON2/app/src/main/java/com/itrw324/lights_on/Database.java
@@ -1,5 +1,6 @@
 package com.itrw324.lights_on;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,12 +13,28 @@ public class Database extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase userDatabase) {
+        userDatabase.execSQL("Create table Users(product_id number primary key,email text,cellphone number,password text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public boolean addUser(int product_id,String email, int cellphone,String password){
+        SQLiteDatabase userdb = this.getWritableDatabase();
+        ContentValues contentValues  = new ContentValues();
+        contentValues.put("Product ID",product_id);
+        contentValues.put("Email",email);
+        contentValues.put("Cellphone",cellphone);
+        contentValues.put("Password",password);
+
+        long insert = userdb.insert("Users",null,contentValues);
+        if (insert==1){
+            return false;
+        }
+        else return true;
 
     }
 }
