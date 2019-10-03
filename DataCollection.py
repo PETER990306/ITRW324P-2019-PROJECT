1cf910d1077a38f79effccdfa68f5b29c2b67187
28713303 <50994852+28713303@users.noreply.github.com>
2019-10-03 13:45:00 +0200

    Update DataCollection.py

--- DataCollection.py
+++ DataCollection.py
@@ -5,8 +5,8 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-API_KEY = '7052ad35e3c73564'
-BASE_URL = "http://api.wunderground.com/api/{}/history_{}/q/NE/Lincoln.json"
+API_KEY = 
+BASE_URL = 
 
 target_date = datetime(2019, 10, 1)
 features = ["date", "meantempm", "maxhumidity", "minhumidity", "maxtempm",
@@ -49,15 +49,12 @@ N = 1
 # target measurement of mean temperature
 feature = 'meantempm'
 
-# total number of rows
+
 rows = tmp.shape[0]
 
-# a list representing Nth prior measurements of feature
-# notice that the front of the list needs to be padded with N
-# None values to maintain the constistent rows length for each N
+
 nth_prior_measurements = [None]*N + [tmp[feature][i-N] for i in range(N, rows)]
 
-# make a new column name of feature_N and add to DataFrame
 col_name = "{}_{}".format(feature, N)
 tmp[col_name] = nth_prior_measurements
 tmp
@@ -75,22 +72,16 @@ def derive_nth_day_feature(df, feature, N):
 			
 			df.columns
 			
-	   Index(['meantempm', 'meandewptm', 'meanpressurem', 'maxhumidity',
-       'minhumidity', 'maxtempm', 'mintempm', 'maxdewptm', 'mindewptm',
-       'maxpressurem', 'minpressurem', 'precipm', 'meantempm_1', 'meantempm_2',
-       'meantempm_3', 'meandewptm_1', 'meandewptm_2', 'meandewptm_3',
-       'meanpressurem_1', 'meanpressurem_2', 'meanpressurem_3',
+	   Index(['meantempm', 'maxhumidity',
+       'minhumidity', 'maxtempm', 'mintempm',
+      
        'maxhumidity_1', 'maxhumidity_2', 'maxhumidity_3', 'minhumidity_1',
        'minhumidity_2', 'minhumidity_3', 'maxtempm_1', 'maxtempm_2',
-       'maxtempm_3', 'mintempm_1', 'mintempm_2', 'mintempm_3', 'maxdewptm_1',
-       'maxdewptm_2', 'maxdewptm_3', 'mindewptm_1', 'mindewptm_2',
-       'mindewptm_3', 'maxpressurem_1', 'maxpressurem_2', 'maxpressurem_3',
-       'minpressurem_1', 'minpressurem_2', 'minpressurem_3', 'precipm_1',
-       'precipm_2', 'precipm_3'],
+       'maxtempm_3', 'mintempm_1', 'mintempm_2', 'mintempm_3', 'maxdewptm_1'],
        dtype='object')
 	   
 	   
-# make list of original features without meantempm, mintempm, and maxtempm
+
 to_remove = [feature 
              for feature in features 
              if feature not in ['meantempm', 'mintempm', 'maxtempm']]
@@ -103,15 +94,9 @@ df = df[to_keep]
 df.columns
 
 Index(['meantempm', 'maxtempm', 'mintempm', 'meantempm_1', 'meantempm_2',
-       'meantempm_3', 'meandewptm_1', 'meandewptm_2', 'meandewptm_3',
-       'meanpressurem_1', 'meanpressurem_2', 'meanpressurem_3',
-       'maxhumidity_1', 'maxhumidity_2', 'maxhumidity_3', 'minhumidity_1',
+       'meantempm_3','maxhumidity_1', 'maxhumidity_2', 'maxhumidity_3', 'minhumidity_1',
        'minhumidity_2', 'minhumidity_3', 'maxtempm_1', 'maxtempm_2',
-       'maxtempm_3', 'mintempm_1', 'mintempm_2', 'mintempm_3', 'maxdewptm_1',
-       'maxdewptm_2', 'maxdewptm_3', 'mindewptm_1', 'mindewptm_2',
-       'mindewptm_3', 'maxpressurem_1', 'maxpressurem_2', 'maxpressurem_3',
-       'minpressurem_1', 'minpressurem_2', 'minpressurem_3', 'precipm_1',
-       'precipm_2', 'precipm_3'],
+       'maxtempm_3', 'mintempm_1', 'mintempm_2', 'mintempm_3',],
       dtype='object') 
 
 df.info()
@@ -124,12 +109,7 @@ mintempm           1000 non-null object
 meantempm_1        999 non-null object
 meantempm_2        998 non-null object
 meantempm_3        997 non-null object
-meandewptm_1       999 non-null object
-meandewptm_2       998 non-null object
-meandewptm_3       997 non-null object
-meanpressurem_1    999 non-null object
-meanpressurem_2    998 non-null object
-meanpressurem_3    997 non-null object
+
 maxhumidity_1      999 non-null object
 maxhumidity_2      998 non-null object
 maxhumidity_3      997 non-null object
@@ -142,13 +122,7 @@ maxtempm_3         997 non-null object
 mintempm_1         999 non-null object
 mintempm_2         998 non-null object
 mintempm_3         997 non-null object
-mindewptm_3        997 non-null object
-maxpressurem_1     999 non-null object
-maxpressurem_2     998 non-null object
-maxpressurem_3     997 non-null object
-minpressurem_1     999 non-null object
-minpressurem_2     998 non-null object
-minpressurem_3     997 non-null object
+
 
 dtypes: object(39)
 memory usage: 312.5+ KB
@@ -183,17 +157,13 @@ mintempm_3         997 non-null float64
 dtypes: float64(36), int64(3)
 memory usage: 312.5 KB
 
-# Call describe on df and transpose it due to the large number of columns
 spread = df.describe().T
 
-# precalculate interquartile range for ease of use in next calculation
 IQR = spread['75%'] - spread['25%']
 
-# create an outliers column which is either 3 IQRs below the first quartile or
-# 3 IQRs above the third quartile
+
 spread['outliers'] = (spread['min']<(spread['25%']-(3*IQR)))|(spread['max'] > (spread['75%']+3*IQR))
 
-# just display the features containing extreme outliers
 spread.ix[spread.outliers,]
 
 %matplotlib inline
@@ -203,16 +173,6 @@ plt.title('Distribution of maxhumidity_1')
 plt.xlabel('maxhumidity_1')
 plt.show()
 
-df.minpressurem_1.hist()
-plt.title('Distribution of minpressurem_1')
-plt.xlabel('minpressurem_1')
-plt.show()
-
-# iterate over the precip columns
-for precip_col in ['precipm_1', 'precipm_2', 'precipm_3']:
-    # create a boolean array of values representing nans
-    missing_vals = pd.isnull(df[precip_col])
-    df[precip_col][missing_vals] = 0
 
 df = df.dropna()
 
