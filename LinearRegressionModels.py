0eb90db05d0bbc93c5db9e15b447ab47fb920f65
28713303 <50994852+28713303@users.noreply.github.com>
2019-10-03 13:09:11 +0200

    Update LinearRegressionModels.py

--- LinearRegressionModels.py
+++ LinearRegressionModels.py
@@ -20,19 +20,15 @@ import numpy as np
 
 %matplotlib inline
 
-# manually set the parameters of the figure to and appropriate size
 plt.rcParams['figure.figsize'] = [16, 22]
 
-# call subplots specifying the grid structure we desire and that 
-# the y axes should be shared
+
 fig, axes = plt.subplots(nrows=6, ncols=3, sharey=True)
 
-# Since it would be nice to loop through the features in to build this plot
-# let us rearrange our data into a 2D array of 6 rows and 3 columns
+
 arr = np.array(predictors).reshape(6, 3)
 
-# use enumerate to loop over the arr 2D array of rows and columns
-# and create scatter plots of each meantempm vs each feature
+
 for row, col_arr in enumerate(arr):
     for col, feature in enumerate(col_arr):
         axes[row, col].scatter(df2[feature], df2['meantempm'])
@@ -42,34 +38,24 @@ for row, col_arr in enumerate(arr):
             axes[row, col].set(xlabel=feature)
 plt.show()
 
-# import the relevant module
-import statsmodels.api as sm
-
-# separate our my predictor variables (X) from my outcome variable y
 X = df2[predictors]
 y = df2['meantempm']
 
-# Add a constant to the predictor variable set to represent the Bo intercept
+
 X = sm.add_constant(X)
 X.ix[:5, :5]
 
-# (1) select a significance value
+
 alpha = 0.05
 
-# (2) Fit the model
 model = sm.OLS(y, X).fit()
 
-# (3) evaluate the coefficients' p-values
+
 model.summary()
 
-# (3) cont. - Identify the predictor with the greatest p-value and assess if its > our selected alpha.
-#             based off the table it is clear that meandewptm_3 has the greatest p-value and that it is
-#             greater than our alpha of 0.05
 
-# (4) - Use pandas drop function to remove this column from X
 X = X.drop('meandewptm_3', axis=1)
-
-# (5) Fit the model 
+ 
 model = sm.OLS(y, X).fit()
 
 model.summary()
@@ -78,22 +64,22 @@ model = sm.OLS(y, X).fit()
 model.summary()
 
 from sklearn.model_selection import train_test_split
-# first remove the const column because unlike statsmodels, SciKit-Learn will add that in for us
+
 X = X.drop('const', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
 
 from sklearn.linear_model import LinearRegression
-# instantiate the regressor class
+
 regressor = LinearRegression()
 
-# fit the build the model by fitting the regressor to the training data
+
 regressor.fit(X_train, y_train)
 
-# make a prediction set using the test set
+
 prediction = regressor.predict(X_test)
 
-# Evaluate the prediction accuracy of the model
+
 from sklearn.metrics import mean_absolute_error, median_absolute_error
 print("The Explained Variance: %.2f" % regressor.score(X_test, y_test))
 print("The Mean Absolute Error: %.2f degrees celsius" % mean_absolute_error(y_test, prediction))
