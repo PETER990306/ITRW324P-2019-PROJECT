e999a6479776d8efe3f44811cb2e59cf16be4308
Teboho-hash <54534135+Teboho-hash@users.noreply.github.com>
2019-10-09 16:03:16 +0200

    Add files via upload

---
+++ mainConnect.java
@@ -0,0 +1,47 @@
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Log_in_Activity extends AppCompatActivity {
+
+ private Button btnEnter;
+  
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnEnter);
+       
+
+       
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Up";
+                Socket_AsyncTask cmd_increase_servo = new Socket_AsyncTask();
+                cmd_increase_servo.execute();
+            }
+        });
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Down";
+                Socket_AsyncTask cmd_increase_servo = new Socket_AsyncTask();
+                cmd_increase_servo.execute();
+            }
+        });
+		
+	}
