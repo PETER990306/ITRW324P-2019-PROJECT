329bd029a960bfe42f745fbe368c76b20e8e0c1e
28713303 <50994852+28713303@users.noreply.github.com>
2019-10-23 21:54:37 +0200

    Update NNModel.py

--- NNModel.py
+++ NNModel.py
@@ -39,15 +39,11 @@ mintempm_3         997 non-null float64
 dtypes: float64(36), int64(3)
 memory usage: 311.6+ KB
 
-
 df = df.drop(['mintempm', 'maxtempm'], axis=1)
-
 
 X = df[[col for col in df.columns if col != 'meantempm']]
 
-
 y = df['meantempm']
-
 
 X_train, X_tmp, y_train, y_tmp = train_test_split(X, y, test_size=0.2, random_state=23)
 
@@ -57,6 +53,7 @@ X_train.shape, X_test.shape, X_val.shape
 print("Training instances   {}, Training features   {}".format(X_train.shape[0], X_train.shape[1]))
 print("Validation instances {}, Validation features {}".format(X_val.shape[0], X_val.shape[1]))
 print("Testing instances    {}, Testing features    {}".format(X_test.shape[0], X_test.shape[1]))
+
 Training instances   797, Training features   36
 Validation instances 100, Validation features 36
 Testing instances    100, Testing features    36
@@ -67,8 +64,6 @@ regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
                                       hidden_units=[50, 50],
                                       model_dir='tf_wx_model')
 									  
-INFO:tensorflow:Using default config.
-INFO:tensorflow:Using config: {'_tf_random_seed': 1, '_save_checkpoints_steps': None, '_save_checkpoints_secs': 600, '_model_dir': 'tf_wx_model', '_log_step_count_steps': 100, '_keep_checkpoint_every_n_hours': 10000, '_save_summary_steps': 100, '_keep_checkpoint_max': 5, '_session_config': None}
 
 def wx_input_fn(X, y=None, num_epochs=None, shuffle=True, batch_size=400):
     return tf.estimator.inputs.pandas_input_fn(x=X,
@@ -77,20 +72,6 @@ def wx_input_fn(X, y=None, num_epochs=None, shuffle=True, batch_size=400):
                                                shuffle=shuffle,
                                                batch_size=batch_size)
 
-regressor.train(input_fn=input_fn(training_data, num_epochs=None, shuffle=True), steps=some_large_number)
-
-.....
-lots of log info
-....
-
-regressor.evaluate(input_fn=input_fn(eval_data, num_epochs=1, shuffle=False), steps=1)
-
-.....
-less log info
-....
-
-
-predictions = regressor.predict(input_fn=input_fn(pred_data, num_epochs=1, shuffle=False), steps=1)
 
 evaluations = []
 STEPS = 400
