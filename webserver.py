c488444e54d5b16ae44958a91914410428a62b55
Teboho-hash <54534135+Teboho-hash@users.noreply.github.com>
2019-10-24 02:04:29 +0100

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,19 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 21570))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  print('Content = %s' % str(request))
+  sensor_readings = read_sensor()
+  print(sensor_readings)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
